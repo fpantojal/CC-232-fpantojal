cb56d3ad128be2b3c85c4f8b223e43edd33e59a2
Fabio Alberto Pantoja Limache <fpantojal@uni.pe>
2026-04-27 21:50:58 -0500

    refactor: rename Bracket.h a BracketStats.h y movi BracketSolver.cpp to src/

---
+++ Libreria_cc232/Semana2/practica-calificada2/src/BracketSolver.cpp
@@ -0,0 +1,23 @@
+#include "BracketStats.h"
+#include <stack>
+#include <string>
+
+namespace PC2
+{
+    // Implementación no optimizada: búsqueda exhaustiva
+    BracketStats solveBracketsNonOptimized(const std::string& s)
+    {
+        BracketStats result;
+        // TODO: Implementación
+        return result;
+    }
+
+    // Implementación optimizada: usando stack
+    BracketStats solveBracketsOptimized(const std::string& s)
+    {
+        BracketStats result;
+        // TODO: Implementación
+        return result;
+    }
+
+} // namespace PC2
